ff85134498df99ed8cbcde78118a0508bbea7d9a
COOCHAN900 <t.daisuke900@outlook.jp>
2024-08-06 16:05:43 +0900

    kadai_mdn_jsフォルダをアップロード

---
+++ kadai_mdn_js/mdn.js
@@ -0,0 +1,12 @@
+// 現在時刻を取得
+const date = new Date();
+
+// 年月日
+const year = date.getFullYear();
+const month = date.getMonth();
+const day = date.getDay();
+
+// 出力表記に合わせた変数
+const formattedDate = `${year}年${month}月${day}日`;
+
+console.log(formattedDate);
